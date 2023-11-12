33bd15514e28e53528022f5b132797eb0f058c9d
Chirag-Solanki6804 <crs279836@gmail.com>
2023-11-12 11:39:26 +0530

    static pages added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Footer from "./Components/Footer";
-import HomeLayout from "./Layouts/HomeLayout";
 import HomePage from "./Pages/HomePage";
+import AboutUs from "./Pages/AboutUs";
+import NotFoundPage from "./Pages/NotFoundPage";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
+        <Route path="/about" element={<AboutUs />}></Route>
+        <Route path="*" element={<NotFoundPage />}></Route>
       </Routes>
     </>
   );
