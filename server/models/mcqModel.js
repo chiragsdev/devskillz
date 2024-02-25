0a8614a3dd25a152ba7854f93d3ca7e6aa73e02f
Chirag-Solanki6804 <crs279836@gmail.com>
2024-02-25 22:46:06 +0530

    Added Mcq model

---
+++ server/models/mcqModel.js
@@ -0,0 +1,35 @@
+import { Schema } from "mongoose";
+
+const mcqSchema = new Schema({
+  question: {
+    type: String,
+    require: [true, "question is required"],
+    trim: true,
+  },
+  options: {
+    type: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+  },
+  correctOptionIndex: {
+    type: Number,
+    required: [true, "Correct option index is required"],
+    min: 1,
+    max: 4,
+  },
+});
+
+// Middleware to validate options array length before saving
+mcqSchema.pre("save", function (next) {
+  if (this.options.length !== 4) {
+    return next(new Error("Exactly 4 options are required"));
+  }
+  next();
+});
+
+const Mcq = model("mcqs", mcqSchema);
+
+export default Mcq;
